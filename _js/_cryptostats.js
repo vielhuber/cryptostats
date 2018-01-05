c500c95c6332367620aab848b6ca7a0be06cf141
David Vielhuber <david@vielhuber.de>
2018-01-06 00:39:48 +0100

    added some charts

--- _js/_cryptostats.js
+++ _js/_cryptostats.js
@@ -1,46 +1,158 @@
 import Helpers from './_helpers';
 import https from 'https';
+import Chart from 'chart.js';
 
 export default class Cryptostats
 {
 
     constructor()
     {
+        this.chart = null;
         this.wallet = {
             'btc': {
                 'addresses': [
                     '1rahL1FtAfausqm54HRPGkRPq6sGs8EJ8',
                     '15QYFNJDd9yiPJc2GummCWNfqHmMERQNLP',
                     '1F7jtuVjirsS8DxVjxxQ2p3FfLGAFyEgx4'
-                ]
+                ],
+                'color': '#EB6534'
             },
             'eth': {
                 'addresses': [
                    '0xedaa09e2998e642da30e98b7693953b655ff5b9e'
-                ]
+                ],
+                'color': '#ACBEA3'
             },
             'ltc': {
                 'addresses': [
                     'LSM2FbpWFYrQ3P6RDyLFBjz7s4mMT3GPfU',
                     'LcUwEFkGNiWNJfkqpygUEN8VL4zVVeEQwP',
                     'LQGj4sbh372GfMZ1SK47tzsQW3354oK4cr'
-                ]
+                ],
+                'color': '#AD5D4E'
             },
             'dash': {
                 'addresses': [
                     'XbYCKYwXnVFxFUL6duqPPWw26aAoj5vs78',
                     'XrYJyZMFkJu1EHNgY5f5tTgwWZ3HKkYb5r'
-                ]
+                ],
+                'color': '#40476D'
             }
         };
     }
 
     init()
     {
+        this.initializeChart();
         this.load().then((result) =>
         {
             console.log('async method done');
         });
+    }
+
+    initializeChart()
+    {
+        this.chart = new Chart(
+            document.querySelector('.chart'),
+            {
+                type: 'line',
+                /*
+                data: {
+                    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                    datasets: [{
+                        label: '# of Votes',
+                        data: [12, 19, 3, 5, 2, 3],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                */
+                data: {
+
+                },
+                options: {
+                    responsive: true,
+                    animation: {
+                        duration: 2000,
+                        easing: 'easeOutCirc'
+                    },
+                    scales: {
+                        xAxes: [{
+                            type: 'time',
+                            time: {
+                                unit: 'day',
+                                unitStepSize: 1,
+                                displayFormats: {
+                                    'day': 'DD.MM.'
+                                }
+                            },
+                            distribution: 'linear',
+                            ticks: {
+                                fontColor: 'white',
+                                showLabelBackdrop: false,
+                                fontStyle: 'bold'
+                            },
+                            pointLabels: {
+                                fontColor: 'white'
+                            },
+                            gridLines: {
+                                color: 'rgba(255, 255, 255, 0.2)'
+                            },
+                            angleLines: {
+                                color: 'white'
+                            }
+                        }],
+                        yAxes: [{
+                            /*
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Wert in €',
+                                fontColor: 'white'
+                            },
+                            */
+                            ticks: {
+                                beginAtZero: true,
+                                fontColor: 'white',
+                                showLabelBackdrop: false,
+                                fontStyle: 'bold',
+                                callback: (value, index, values) => { return value+' €'; }
+                            },
+                            pointLabels: {
+                                fontColor: 'white'
+                            },
+                            gridLines: {
+                                color: 'rgba(255, 255, 255, 0.2)'
+                            },
+                            angleLines: {
+                                color: 'white'
+                            }
+                        }]
+                    },
+                    legend: {
+                      position: 'top',
+                      labels: {
+                        fontColor: 'white',
+                        fontStyle: 'bold'
+                      }
+                    },
+                }
+            }
+        );
     }
 
     async load()
@@ -51,38 +163,83 @@ export default class Cryptostats
             // loop through all addresses
             for(let address of data.addresses)
             {
-                let balance = 0;
-                let api = await this.getBalance(coin, address).catch((error) => {});
-                if( api && api.final_balance )
-                {
+                let balance;
+                let api = await this.getBalance(coin, address).then((data) => {
                     balance = api.final_balance;
-                }
+                }).catch((error) => {
+                    balance = ~~(Math.random()*(10000000-1000000+1))+1000000;
+                    if( coin === 'btc' ) { balance *= 0.05; }
+                    if( coin === 'eth' ) { balance *= 10000000000; }
+                });
                 this.setBalance(coin, address, balance);          
             }
 
             // balances of coin is finished: get market cap of this coin (don't await)
             let marketcap = this.getMarketCap(coin).catch(error => console.log(error)).then((data) => 
             {
-                this.drawChart(coin, data.Data);
+                this.addToChart(coin, data.Data);
+                // if this was last
+                if( this.chart.data.datasets.length === Object.keys(this.wallet).length )
+                {
+                    // throttle
+                    setTimeout(() => 
+                    {
+                        this.sumUpData();
+                    },2000);
+                    console.log(this.wallet);
+                }
             });
         }
-        // all finished
-        console.log(this.wallet);
     }
 
-    drawChart(coin, data)
+    sumUpData()
     {
-        if( this.wallet[coin].chart === undefined )
+        let dataset = {
+            label: '*',
+            backgroundColor: '#ffffff10',
+            borderColor: '#ffffff',
+            fill: true,
+            data: []
+        }
+        this.chart.data.datasets[0].data.forEach((data__value, data__key) =>
         {
-            this.wallet[coin].chart = [];
+            let sum = 0;
+            for( let dataset__value of this.chart.data.datasets )
+            {
+                sum += dataset__value.data[data__key].y;
+            }
+            sum = (Math.round(sum*100)/100);
+            dataset.data.push({
+                t: data__value.t,
+                y: sum
+            });
+        });
+        this.chart.data.datasets.push(dataset);
+        this.chart.update();
+    }
+
+    addToChart(coin, data)
+    {
+        let dataset = {
+            label: coin.toUpperCase(),
+            backgroundColor: this.wallet[coin].color+'10', // hexa color
+            borderColor: this.wallet[coin].color,
+            fill: true,
+            data: []
         }
         for(let data__value of data)
         {
-            this.wallet[coin].chart.push({
+            dataset.data.push({
                 t: new Date(data__value.time * 1000),
                 y: this.calcFinalValue(coin, data__value.close)
             });
         }
+        if( this.chart.data.datasets.length === 0 )
+        {
+            document.querySelector('.chart').classList.remove('chart--hidden');
+        }
+        this.chart.data.datasets.push(dataset);
+        this.chart.update();
     }
 
     calcFinalValue(coin, value)
